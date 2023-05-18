bbd1da3c39853728858dc6e73683022d003869bb
Afnan <avnanrahman@gmail.com>
2023-05-18 23:56:38 +0700

    add name and address

--- src/config/database.js
+++ src/config/database.js
@@ -14,4 +14,13 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+// Create a user table if it doesn't exist
+pool.query(`CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL
+)`).catch(error => console.error('Failed to create users table', error));
+
 module.exports = pool;
--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -5,13 +5,13 @@ const pool = require('../config/database');
 
 async function register(req, res) {
   try {
-    const { email, password } = req.body;
+    const { email, password, name, address } = req.body;
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert the user into the database
-    await pool.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hashedPassword]);
+    await pool.query('INSERT INTO users (email, password, name, address) VALUES (?, ?, ?, ?)', [email, hashedPassword, name, address]);
 
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
