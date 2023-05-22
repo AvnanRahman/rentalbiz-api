28ecb1be2682b87641b4cf5350688ca3daa7e3cc
Afnan <avnanrahman@gmail.com>
2023-05-23 00:31:23 +0700

    edit env

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env._DB_HOST,
+  user: process.env._DB_USER,
+  password: process.env._DB_PASSWORD,
+  database: process.env._DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
