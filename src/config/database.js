de33d285b38e1e42fd0d46cd5b4f405068fb9563
Afnan <avnanrahman@gmail.com>
2023-05-23 13:03:35 +0700

    try unix socket

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,37 @@
 // src/config/database.js
 
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
+
+const mysql = require('promise-mysql');
 require('dotenv').config();
 
 // Create a connection pool
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+
+// createUnixSocketPool initializes a Unix socket connection pool for
+// a Cloud SQL instance of MySQL.
+const pool = async config => {
+  // Note: Saving credentials in environment variables is convenient, but not
+  // secure - consider a more secure solution such as
+  // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+  // keep secrets safe.
+  return mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
+    // Specify additional properties here.
+  });
+};
 
 // Create a user table if it doesn't exist
 pool.query(`CREATE TABLE IF NOT EXISTS users (
